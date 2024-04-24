73e5a6f0d92971dfb666b4d6037fcb0d817f1bcb
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-04-24 20:19:03 +0700

    setup_structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <div>
+      hello huyhoang-doit
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/ExerciseDetail.jsx
@@ -0,0 +1,10 @@
+
+const ExerciseDetail = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ExerciseDetail
